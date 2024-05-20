21de64e029592f4fc819184e0053a7789dbc6c61
Edanriell <Edanriell@gmail.com>
2024-05-20 12:51:16 +0300

    working on boxy type buttons

--- client/src/shared/ui/button/ui.tsx
+++ client/src/shared/ui/button/ui.tsx
@@ -15,13 +15,13 @@ type ButtonProps = {
 	VariantProps<typeof buttonTypes>;
 
 const buttonTypes = cva(
-	"font-manrope font-bold text-[1.6rem] min-[1440px]:text-[1.8rem] tracking-[-0.01em] leading-[200%] min-[1440px]:leading-[178%]",
+	"font-manrope font-bold text-[1.6rem] min-[1440px]:text-[1.8rem] tracking-[-0.01em] leading-[200%] min-[1440px]:leading-[178%] relative",
 	{
 		variants: {
 			buttonType: {
 				underlined:
-					"text-white-50 after:block after:w-full after:h-[0.3rem] after:bg-spring-green-400 after:absolute relative",
-				boxy: "text-violet-950 bg-spring-green-400 pt-[1.1rem] min-[1440px]:pt-[1.3rem] pb-[1.2rem] min-[1440px]:pb-[1.6rem] px-[2.3rem] min-[1440px]:px-[3.2rem]"
+					"text-white-50 after:block after:w-full after:h-[0.3rem] after:bg-spring-green-400 after:absolute",
+				boxy: "text-violet-950 bg-transparent pt-[0.9rem] min-[1440px]:pt-[1.1rem] pb-[1rem] min-[1440px]:pb-[1.4rem] px-[2.1rem] min-[1440px]:px-[3rem] border-[0.2rem] border-solid border-spring-green-400"
 			}
 		}
 	}
@@ -44,9 +44,16 @@ export const Button: FC<ButtonProps> = ({ buttonType, className, children, ...pr
 			);
 		case "boxy":
 			return (
-				<button type={"button"} className={cn(buttonTypes({ buttonType, className }))} {...props}>
-					{children}
-				</button>
+				<motion.button
+					type={"button"}
+					className={cn(buttonTypes({ buttonType, className }))}
+					{...props}
+					whileHover={{ color: "#44ffa1", scale: 1.05 }}
+					whileTap={{ color: "#44ffa1", scale: 0.95 }}
+				>
+					<div className={"relative z-20"}>{children}</div>
+					<div className={"absolute top-0 left-0 w-full h-full bg-spring-green-400 z-10"}></div>
+				</motion.button>
 			);
 		default:
 			return null;
